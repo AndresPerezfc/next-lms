9cfd0a5665ada91be8e962c0b3c2d1e226c0b223
andresperezfc <felipe3apc@gmail.com>
2024-03-14 21:15:47 -0500

    Course & Chapters Layout

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -1,4 +1,10 @@
+import { auth } from "@clerk/nextjs";
 import { Chapter, Course, UserProgress } from "@prisma/client";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { CourseSideBarItem } from "./course-sidebar-item";
 
 interface CourseSidebarProps {
   course: Course & {
@@ -9,9 +15,43 @@ interface CourseSidebarProps {
   progressCount: number;
 }
 
-export const CourseSidebar = ({
+export const CourseSidebar = async ({
   course,
   progressCount,
 }: CourseSidebarProps) => {
-  return <div>Sidebar!</div>;
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const purchase = await db.purchase.findUnique({
+    where: {
+      userId_courseId: {
+        userId,
+        courseId: course.id,
+      },
+    },
+  });
+
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
+      <div>
+        <h1>{course.title}</h1>
+        {/* Check purchase and add progress */}
+      </div>
+      <div className="flex flex-col w-full">
+        {course.chapters.map((chapter) => (
+          <CourseSideBarItem
+            key={chapter.id}
+            id={chapter.id}
+            label={chapter.title}
+            isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+            courseId={course.id}
+            isLocked={!chapter.isFree && !purchase}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
