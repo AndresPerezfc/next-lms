372d4a6fe6285e775963384a61ea0bb796130b6e
Andres Pereze <felipe3apc@gmail.com>
2024-03-27 22:18:47 -0500

    Chapter progress functionality - Part 1

---
+++ components/course-progress.tsx
@@ -0,0 +1,31 @@
+import { Progress } from "@/components/ui/progress";
+
+interface CourseProgressProps {
+    value: number;
+    variant: "default" | "success",
+    size?: "default" | "sm";
+};
+
+
+const colorByVariant ={
+    default: "text-sky-700",
+    success: "text-emerald-700"
+}
+
+const sizeByVariant ={
+    default: "text-sm",
+    sm: "text-xs"
+}
+
+
+export const CourseProgress = ({
+    value,
+    variant,
+    size
+}: CourseProgressProps) =>{
+
+
+    return (
+        <Progress className="h-2" value={value} />
+    )
+}
