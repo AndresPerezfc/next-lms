e8d8d861018e2404fe72cde92677fc5a65c31875
Andres Pereze <felipe3apc@gmail.com>
2024-04-04 22:57:09 -0500

    Cambio de comilla simple a &apos;

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -53,7 +53,7 @@ const CreatePage = () => {
       <div>
         <h1 className="text-2xl">Name your course</h1>
         <p className="text-sm text-slate-600">
-          what would you like to name your course? don't worry, you can change
+          what would you like to name your course? don&apos;t worry, you can change
           later.
         </p>
         <Form {...form}>
